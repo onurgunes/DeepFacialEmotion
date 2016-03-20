bc064315de31bee378676066edb2748dcbc06c8d
Onur Güneş <gns.onr@gmail.com>
2016-03-20 19:16:07 +0200

    video captured

---
+++ run.lua
@@ -0,0 +1,39 @@
+
+-- include required packages
+require 'xlua'	
+require 'qt'
+require 'qtwidget'
+require 'qtuiloader'
+xrequire('nnx',true)
+xrequire('camera',true)
+
+-- setup UI
+
+widget = qtuiloader.load('gui.ui')
+win1 = qt.QtLuaPainter(widget.frame)
+
+-- initializing the camera
+
+camera = image.Camera{}
+
+function display()
+  cam = torch.Tensor(100,100)
+	cam = camera:forward()	-- takes image from camera
+	image.display{image = cam, win = win1, zoom = 0.75}	--original image
+end
+
+-- timer
+timer = qt.QTimer()
+timer.interval = 10
+timer.singleShot = true
+qt.connect(timer,
+  'timeout()',
+  function() 
+    display()
+    collectgarbage()
+    timer:start()
+  end)
+
+widget.windowTitle = 'Deep Facial Emotion Detector'
+widget:show()
+timer:start()
