9d1756dab24dd413ce9e83e01ac4aa8f9693cbb2
Onur Güneş <gns.onr@gmail.com>
2016-03-20 22:49:31 +0200

    train and test functions was added

--- run.lua
+++ run.lua
@@ -8,18 +8,26 @@ xrequire('nnx',true)
 xrequire('camera',true)
 
 -- setup UI
-
 widget = qtuiloader.load('gui.ui')
 win1 = qt.QtLuaPainter(widget.frame)
 
 -- initializing the camera
-
 camera = image.Camera{}
 
 function display()
   cam = torch.Tensor(100,100)
 	cam = camera:forward()	-- takes image from camera
 	image.display{image = cam, win = win1, zoom = 0.75}	--original image
+end
+
+-- train network
+function train(dataset)
+
+end
+
+-- test network
+function test(dataset)
+
 end
 
 -- timer
